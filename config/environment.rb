a82b8a1bb84dbf29e419facf4789b3f61e9ec101
MesWinDu <math7064@gmail.com>
2023-07-28 13:21:11 +0000

    update

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,10 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+    
+    # Allow connections to local server on cloud IDE.
+    config.hosts.clear
+    end
+    
